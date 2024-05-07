136df0d0e726223923e2349c0f13e4c737fde309
agathemczk <agathe.maciaszczyk@icloud.com>
2024-05-07 09:57:08 +0200

    exercice 2

---
+++ exercie_2.c
@@ -0,0 +1,25 @@
+//Objectif : Définir une structure Personne contenant le nom, l'âge et la taille. Écrire un
+//programme qui crée un tableau de Personne, initialise les données via des
+//        pointeurs, et affiche les données de chaque personne.
+
+# include <stdio.h>
+
+typedef struct person {
+    char name[50];
+    int age;
+    float size;
+} person ;
+
+int main () {
+    person Person[3] = {{"Martin", 24, 1.93}, {"Emma", 42, 1.68}, {"Sacha", 34, 1.76}} ;
+    person *ptr = Person ;
+
+    for(int i = 0; i<3; i++) {
+        printf("\nNom : %s \n",(ptr+i)->name);
+        printf("Age : %d ans \n", (ptr+i)->age);
+        printf("Taille : %.2fm\n", (ptr+i)->size);
+    }
+
+    return 0;
+
+}
--- main.c
+++ main.c
@@ -1,22 +1,10 @@
-// Écrire un programme qui déclare deux variables numériques. Utiliser des
-// pointeurs pour échanger les valeurs de ces deux variables sans utiliser de variable temporaire.
+//Objectif : Définir une structure Personne contenant le nom, l'âge et la taille. Écrire un
+//programme qui crée un tableau de Personne, initialise les données via des
+//        pointeurs, et affiche les données de chaque personne.
 
 # include <stdio.h>
 
-int main() {
-    int first_value = 4;
-    int second_value = 17;
-    int *ptr1 = &first_value;
-    int *ptr2 = &second_value;
 
-    printf (" Valeur 1 : %d            Valeur 2 : %d\n", first_value, second_value);
-
-    *ptr1 = *ptr1 + *ptr2 ;
-    *ptr2 = *ptr1 - *ptr2 ;
-    *ptr1 = *ptr1 - *ptr2 ;
-
-    printf (" Valeur 1 : %d            Valeur 2 : %d\n", first_value, second_value);
-
+int main () {
     return 0;
 }
-
